9c5bb6e9a0a19fc2112709cc04b57347c38d937c
KeRo99 <motrilwireless@gmail.com>
2010-11-04 00:50:45 +0100

    Crashfix, add check in Addthreat

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -9128,6 +9128,9 @@ float Unit::ApplyTotalThreatModifier(float threat, SpellSchoolMask schoolMask)
 
 void Unit::AddThreat(Unit* pVictim, float threat /*= 0.0f*/, bool crit /*= false*/, SpellSchoolMask schoolMask /*= SPELL_SCHOOL_MASK_NONE*/, SpellEntry const *threatSpell /*= NULL*/)
 {
+	if(!pVictim || !pVictim->isAlive())
+        return;
+
     // Only mobs can manage threat lists
     if(CanHaveThreatList())
         m_ThreatManager.addThreat(pVictim, threat, crit, schoolMask, threatSpell);
