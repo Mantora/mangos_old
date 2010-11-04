0bc9b7812cdbd6dac26d62ca858f90e5d370258d
selector <limurchick@gmail.com>
2010-11-05 00:31:27 +0100

    [C8] fix Judgment of Light procs

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -2104,6 +2104,14 @@ SpellAuraProcResult Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage, Aura
                     if (pVictim == this)
                        return SPELL_AURA_PROC_FAILED;
 
+					// PPM per victim
+					float ppmJoL = 15.0f; // must be hard-coded + 100% proc chance in DB
+					WeaponAttackType attType = BASE_ATTACK; // TODO: attack type based? 
+					uint32 WeaponSpeed = pVictim->GetAttackTime(attType);
+					float chanceForVictim = pVictim->GetPPMProcChance(WeaponSpeed, ppmJoL);
+					if (!roll_chance_f(chanceForVictim))
+						return false;
+
                     basepoints[0] = int32( pVictim->GetMaxHealth() * triggeredByAura->GetModifier()->m_amount / 100 );
                     pVictim->CastCustomSpell(pVictim, 20267, &basepoints[0], NULL, NULL, true, NULL, triggeredByAura);
                     return SPELL_AURA_PROC_OK;
