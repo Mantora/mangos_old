b3f92563d11d801d00b6dda36c4dd49fee07b72c
Berni <Berni@.(none)>
2010-11-19 17:54:30 +0100

    Revert "[S095] Add VIP table and commands(custom feature):"
    This reverts commit de36fb854d8f9c4ec5ef02ed64b4254573b1d0df.

--- addition/stcore_addition/S095_mangos_vip_commands.sql
+++
@@ -1,8 +0,0 @@
--- Custom feature, read syntax for explonation. 
-REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('vip debuff',1,'Syntax: .vip debuff - removes Resurrection Sickness and Deserter debuffs from owner');
-REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('vip map', 1, 'Syntax: .vip map - reveals all maps to owner');
-REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('vip bank', 1, 'Syntax: .vip bank - Show your bank inventory.');
-REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('vip repair', 1, 'Syntax: .vip repair - repair all your items.');
-REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('vip auction', 1, 'Syntax: .vip auction - opens auctionhouse window to character.');
-REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('vip resettalents', 1, 'Syntax: .vip resettalents - reset talents to your character.');
-REPLACE INTO `command` (`name`,`security`,`help`) VALUES ('vip whispers', 1, 'Syntax: .vip whispers - Enable/disable accepting whispers by VIP accounts from players.');
