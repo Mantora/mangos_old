b2849e11a497bc891435299be5fcb72da4eae6c6
Berni <Berni@.(none)>
2010-11-22 21:59:30 +0100

    global and per-map config options added to mangosd.conf - default is enabled
    * Enable/disable mmaps possible with .reload conf
    * mmap data always loads if available, but only used if enabled
      move, rename, or delete mmap directory/files to prevent load

--- src/game/Map.h
+++ src/game/Map.h
@@ -38,6 +38,7 @@
 
 #include <bitset>
 #include <list>
+#include <set>
 
 #include "../recastnavigation/Detour/Include/DetourNavMesh.h"
 
@@ -325,6 +326,31 @@ class MANGOS_DLL_SPEC Map : public GridRefManager<NGridType>, public MaNGOS::Obj
 
         template<class T>
             void DeleteFromWorld(T*);
+
+        // begin movemap-related
+    public:
+        dtNavMesh* GetNavMesh();
+        static void preventPathfindingOnMaps(std::string ignoreMapIds);
+        bool IsPathfindingEnabled() const;
+
+    private:
+        void LoadNavMesh(int gx, int gy);
+        void UnloadNavMesh(int gx, int gy);
+        dtNavMesh* m_navMesh;
+        UNORDERED_MAP<uint32, uint32> m_mmapTileMap;    // maps [map grid coords] to [dtTile coords]
+
+        static std::set<uint32> m_mmapDisabledIds;      // stores list of mapids which do not use pathfinding
+        // end movemap-related
+};
+
+enum InstanceResetMethod
+{
+    INSTANCE_RESET_ALL,
+    INSTANCE_RESET_CHANGE_DIFFICULTY,
+    INSTANCE_RESET_GLOBAL,
+    INSTANCE_RESET_GROUP_DISBAND,
+    INSTANCE_RESET_GROUP_JOIN,
+    INSTANCE_RESET_RESPAWN_DELAY
 };
 
 class MANGOS_DLL_SPEC InstanceMap : public Map
