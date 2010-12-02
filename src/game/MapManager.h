de99285056dfa10db1676399d38bd6b8729fe052
selector <limurchick@gmail.com>
2010-12-02 11:50:23 +0200

    [ACE001] ACE MTMaps thx to kero99 for adapting

--- src/game/MapManager.h
+++ src/game/MapManager.h
@@ -25,6 +25,7 @@
 #include "ace/Recursive_Thread_Mutex.h"
 #include "Map.h"
 #include "GridStates.h"
+#include "MapUpdater.h"
 
 class Transport;
 class BattleGround;
@@ -175,6 +176,7 @@ class MANGOS_DLL_DECL MapManager : public MaNGOS::Singleton<MapManager, MaNGOS::
         MapMapType i_maps;
 
         IntervalTimer i_timer;
+        MapUpdater m_updater;
 };
 
 #define sMapMgr MapManager::Instance()
