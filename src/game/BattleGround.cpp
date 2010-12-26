cded6f9a4bb77397dbacc60c85a43a763edf1bf8
selector <limurchick@gmail.com>
2010-12-26 20:24:16 +0200

    [S137] For arena rating below a limit do not lose rating when team loses match
    original author: as I remember - KillerFrca

--- src/game/BattleGround.cpp
+++ src/game/BattleGround.cpp
@@ -765,6 +765,13 @@ void BattleGround::EndBattleGround(Team winner)
             winner_rating = winner_arena_team->GetStats().rating;
             int32 winner_change = winner_arena_team->WonAgainst(loser_rating);
             int32 loser_change = loser_arena_team->LostAgainst(winner_rating);
+            uint32 noratinglimit = sWorld.getConfig(CONFIG_UINT32_LOSERNOCHANGE);
+            uint32 halfratinglimit = sWorld.getConfig(CONFIG_UINT32_LOSERHALFCHANGE);
+
+            if(winner_rating <= noratinglimit || loser_rating <= noratinglimit)
+                loser_change = 0;
+            else if (loser_rating <= halfratinglimit)
+                loser_change /= 2;
             DEBUG_LOG("--- Winner rating: %u, Loser rating: %u, Winner change: %i, Loser change: %i ---", winner_rating, loser_rating, winner_change, loser_change);
             SetArenaTeamRatingChangeForTeam(winner, winner_change);
             SetArenaTeamRatingChangeForTeam(GetOtherTeam(winner), loser_change);
