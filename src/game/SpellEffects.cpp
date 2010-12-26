9a7f13e7bb591df156f637fe8542822ff649351e
Berni <andislittlefighter2@arcor.de>
2010-12-26 20:12:43 +0100

    [C54] increased fall damage | Unit::KnockBackFrom thx x3n

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -8631,7 +8631,7 @@ void Spell::EffectKnockBack(SpellEffectIndex eff_idx)
         if (m_caster->HasAura(62126)) // Glyph of Blast Wave
             return;
 
-    unitTarget->KnockBackFrom(m_caster,float(m_spellInfo->EffectMiscValue[eff_idx])/10,float(damage)/30);
+    unitTarget->KnockBackFrom(m_caster,float(m_spellInfo->EffectMiscValue[eff_idx])/10,float(damage)/10);
 }
 
 void Spell::EffectSendTaxi(SpellEffectIndex eff_idx)
