4ddf7d698b898925046cbfc8c8cc82373296d721
Berni <andislittlefighter2@arcor.de>
2010-12-30 01:41:21 +0100

    [C61] function to get go from bg thx Feanordev

--- src/game/BattleGround.h
+++ src/game/BattleGround.h
@@ -454,6 +454,8 @@ class BattleGround
         void SendYellToAll(int32 entry, uint32 language, ObjectGuid guid);
         void PSendMessageToAll(int32 entry, ChatMsg type, Player const* source, ...  );
 
+        GameObject* GetBGObject(uint32 type);
+
         // specialized version with 2 string id args
         void SendMessage2ToAll(int32 entry, ChatMsg type, Player const* source, int32 strId1 = 0, int32 strId2 = 0);
         void SendYell2ToAll(int32 entry, uint32 language, ObjectGuid guid, int32 arg1, int32 arg2);
